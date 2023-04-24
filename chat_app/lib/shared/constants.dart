9c9e963c2bef8e63260b5cf60ae7beef2fecc5a6
Al Sayer <116816897+HSayer44@users.noreply.github.com>
2023-04-24 20:33:28 +0200

    Update constants.dart

--- chat_app/lib/shared/constants.dart
+++ chat_app/lib/shared/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Constants {
-  static String appID = "1:528000346330:web:1bd581d4f5a36948411de3";
-  static String apiKey = "AIzaSyCEYzF1TmMTYowr5jD8Ch-mQlwhoNzGHj4";
-  static String messagingSenderId = "528000346330";
+  static String appID = "YOUR_APP_ID";
+  static String apiKey = "YOUR_API_KEY";
+  static String messagingSenderId = "MESSAGE_SENDER_ID";
   static String projectId = "chatappflutter-2ef64";
   final primaryColor = Colors.blue;
 
